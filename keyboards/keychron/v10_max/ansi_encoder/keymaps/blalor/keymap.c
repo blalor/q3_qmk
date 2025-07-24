428d943cc9fa114af19a1344fd138205633f0689
Brian Lalor <blalor@bravo5.org>
2025-07-24 06:26:05 -0400

    Moving some keys around to match my muscle memory a bit better

--- keyboards/keychron/v10_max/ansi_encoder/keymaps/blalor/keymap.c
+++ keyboards/keychron/v10_max/ansi_encoder/keymaps/blalor/keymap.c
@@ -55,18 +55,18 @@ tap_dance_action_t tap_dance_actions[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-        rot btn     Esc       F1        F2        F3        F4        F5         F6        F7        F8        F9        F10       F11       F12       Ins                           Del
-        M1          `         1         2         3         4         5          6         7         8         9         0         -         =         ⌫                             Pgup
-        M2          →         Q         W         E         R         T          Y         U         I         O         P         [         ]                   \                   Pgdn
-        M3          Caps      A         S         D         F         G          H         J         K         L         ;         '                   ⏎                             Home
+        rot btn     Esc       F1        F2        F3        F4        F5         F6        F7        F8        F9        F10       F11       F12       Del                           Pgup
+        M1          `         1         2         3         4         5          6         7         8         9         0         -         =         ⌫                             Pgdn
+        M2          →         Q         W         E         R         T          Y         U         I         O         P         [         ]                   \                   Home
+        M3          Caps      A         S         D         F         G          H         J         K         L         ;         '                   ⏎                             End
         M4          ⇧                   Z         X         C         V          B         B         N         M         ,         .         /         ⇧                   Up
         M5          ^         ⌥                   ⌘         Space     fn                             Space               ⌘                                       Left      Down      Rght
     */
     [BASE] = LAYOUT_ansi_89(
-        KC_MPLY,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,                       KC_DEL,
+        KC_MPLY,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,                       KC_HOME,
         _CM,        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,                      KC_PGUP,
         MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_BSLS,            KC_PGDN,
-        MC_3,       KC_ESC ,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                       KC_HOME,
+        MC_3,       KC_ESC ,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                       KC_END,
         MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_NO,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,
         MO(_L2),    KC_LCTL,  KC_LOPTN,           KC_LCMMD, KC_SPC,   MO(_L1),                       KC_SPC,             KC_RCMMD,                               KC_LEFT,  KC_DOWN,  KC_RGHT),
 
