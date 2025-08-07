3ac8e76a2aaec4f7964aea7acdc84a2449562627
Brian Lalor <blalor@bravo5.org>
2025-08-06 21:39:38 -0400

    Adding per-key lighting control

--- keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/blalor/rules.mk
+++ keyboards/keychron/k15_max/ansi_encoder/rgb/keymaps/blalor/rules.mk
@@ -7,3 +7,10 @@ OPT_DEFS += -UAPDAPTIVE_NKRO_ENABLE
 
 VIA_ENABLE = no
 TAP_DANCE_ENABLE = yes
+
+RGB_MATRIX_LEDMAPS = yes
+
+ifeq ($(strip $(RGB_MATRIX_LEDMAPS)), yes)
+	SRC += rgb_matrix_ledmaps.c
+	OPT_DEFS += -DRGB_MATRIX_LEDMAPS_ENABLED
+endif
