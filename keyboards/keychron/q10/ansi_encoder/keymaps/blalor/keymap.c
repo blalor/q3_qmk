091dd227a8e66ebb13fdc687101c87131556bb2b
Brian Lalor <blalor@bravo5.org>
2025-11-30 12:27:37 -0500

    Adding globe key to q10

--- keyboards/keychron/q10/ansi_encoder/keymaps/blalor/keymap.c
+++ keyboards/keychron/q10/ansi_encoder/keymaps/blalor/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _CM,        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,                      KC_PGUP,
         MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_BSLS,            KC_PGDN,
         MC_3,       KC_ESC ,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                       KC_END,
-        MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_NO,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,
+        MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_GLOBE, KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,
         MO(_L2),    KC_LCTL,  KC_LOPT,            KC_LCMD,  KC_SPC,   MO(_L1),                       KC_SPC,             KC_RCMD,                                KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_L1] = LAYOUT_ansi_89(
